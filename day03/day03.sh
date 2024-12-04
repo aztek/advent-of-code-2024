4766b39cc9a14a7581263f6bdcc6c7c6d40e3368
Evgenii Kotelnikov <evgenii.kotelnikov@snyk.io>
2024-12-04 23:01:40 +0100

    Awk code golf

--- day03/day03.sh
+++ day03/day03.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 part1() {
-  grep -Eo 'mul\(\d{1,3},\d{1,3}\)' | tr -d 'mul()' | tr , '*' | \
-  paste -s -d+ - | bc
+  grep -Eo 'mul\(\d{1,3},\d{1,3}\)' | tr -d 'mul()' | tr , '*' | paste -s -d+ - | bc
 }
 
 part2() {
   grep -Eo "mul\(\d{1,3},\d{1,3}\)|do(n't)?\(\)" | tr -d 'mul()' | tr , '*' | \
-  awk '$0 == "do" { skip = 0 } $0 == "don'\''t" { skip = 1 } !skip { print }' | \
-  paste -s -d+ - | bc
+  awk '/do/ { skip = 0 } /don'\''t/ { skip = 1 } !skip' | paste -s -d+ - | bc
 }
 
 tee >(part1 1>&2) | part2 2>&1
